e922a2cc7b0bdb00f89a1291d84e7b6d720e5896
edazizovv <edazizovv@gmail.com>
2024-09-11 01:38:56 +0300

    Great ReOrg

---
+++ README.md
@@ -0,0 +1,23 @@
+# x_1000
+
+<a target="_blank" href="https://cookiecutter-data-science.drivendata.org/">
+    <img src="https://img.shields.io/badge/CCDS-Project%20template-328F97?logo=cookiecutter" />
+</a>
+
+A modelling pack: linear
+
+## STATUS:
+
+Operational
+
+## MIRO
+
+<a target="_blank" href="enter miro link">
+    Miro
+</a>
+
+## AZURE
+
+<a target="_blank" href="enter azure link">
+    Azure
+</a>
--- blade_runner.py
+++ blade_runner.py
@@ -44,6 +44,9 @@ model_name = 'OLS'
 model_kwargs = {'cov_type': 'nonrobust'}
 # model_kwargs = {'cov_type': 'HC3'}
 # model_kwargs = {'cov_type': 'HAC', 'cov_kwds': {'maxlags': 1}}
+
+# model_name = 'WLS'
+# model_kwargs = {}
 # model_kwargs = {'weights_finder': 'fitted_resids'}
 # model_kwargs = {'weights_finder': 'abs(err)'}
 
@@ -51,3 +54,79 @@ diagnosed = ammo.diagnose(x=x, y=y, model_code=model_name, x_factors=x_factors, 
 fitted = ammo.fit(x=x, y=y, model_code=model_name, x_factors=x_factors, model_kwargs=model_kwargs)
 assessed = ammo.assess(x=x, y=y, model=fitted.model, x_factors=x_factors)
 measured = ammo.measure(x=x, y=y, model=fitted.model)
+
+'''
+Available methods:
+
+diagnosed.values() returns:
+        > multicollinearity: condition number
+        > multicollinearity: Pearson: correlated pairs
+        > multicollinearity: Pearson: correlation matrix
+        > multicollinearity: Kendall Tau: correlated pairs
+        > multicollinearity: Kendall Tau: correlation matrix
+        > multicollinearity: VIF: estimations
+        > multicollinearity: VIF: models built [TBD]
+diagnosed.summary() prints:
+        Multicollinearity:
+            -- condition number
+            -- Pearson & Kendall Tau correlated pairs (including significance)
+            -- VIF estimations
+diagnosed.plot() plots:
+        > multicollinearity: Pearson & Kendall Tau correlation matrices colorized
+
+
+fitted.values() returns:
+        model coefficients (starting with intercept)
+fitted.summary() prints:
+        model formula
+fitted.plot() is not implemented
+
+
+assessed.values() returns:
+        Summary tables and tests:
+            Summary table 1:
+                > linear specification adequacy: aggregated - h0, n passed, significance thresh
+                > error terms' distribution
+                    -- zero-mean: aggregated - h0, n passed, significance thresh
+                    -- normality: aggregated - h0, n passed, significance thresh
+                    -- homoskedasticity: aggregated - h0, n passed, significance thresh
+                    -- absence of autocorrelation: h0, value, thresh rule
+            Summary table 2:
+                > individual significance: h0, factor name, pvalue
+                > overall model significance: h0, model, pvalue
+            All tests' original classes
+            All significance values
+        Linear Specification test
+        Zero Mean test
+        Normality tests
+        Homoskedasticity tests
+        Autocorrelation test
+        Individual factor significance
+        Group factor significance
+assessed.summary() prints:
+        Part 1:
+            > linear specification adequacy: aggregated - h0, n passed, significance thresh
+            > error terms' distribution
+                -- zero-mean: aggregated - h0, n passed, significance thresh
+                -- normality: aggregated - h0, n passed, significance thresh
+                -- homoskedasticity: aggregated - h0, n passed, significance thresh
+                -- absence of autocorrelation: h0, value, thresh rule
+        Part 2:
+            > individual significance: h0, factor name, pvalue
+            > overall model significance: h0, model, pvalue
+assessed.plot() plots:
+        > y: errors // x: n_ob; bounded by outlier border
+        > hist: y; bounded by outlier border
+        > hist: errors; bounded by outlier border
+        > influence/outlier plot
+        > y: errors // x: y; bounded by outlier borders for each
+
+measured.values() returns:
+    dataframe with the summary
+measured.summary() prints:
+    estimated values for:
+        NSE (standardized MSE, actually R2), higher is better
+        NAE (standardized MAE), higher is better
+        SMAPE, lower is better
+measured.plot() is not implemented
+'''
